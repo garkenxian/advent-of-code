13d719fc436ecf7006a2190b41c30f2a5616075e
kenneth.garza <kenneth.garza@workday.com>
2025-12-04 10:29:06 -0500

    2025 day 4 part 2

---
+++ 2025/python/day4/p2.py
@@ -0,0 +1,70 @@
+import os
+import copy
+
+test_mode = False
+
+fn = "_test.txt" if test_mode else "_input.txt"
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, fn)
+
+with open(file_path, 'r') as file:
+  content = file.read()
+
+arr = []
+for line in content.split("\n"):
+  if line != "":
+    arr.append(list(line))
+
+move_arr = copy.deepcopy(arr)
+
+width = len(arr[0])
+length = len(arr)
+
+movable_count = 0
+keep_going = True
+
+while(keep_going):
+  keep_going = False
+
+  print("== Going around for another pass ==")
+
+  for l in range(0, length):
+    for w in range(0, width):
+      item = arr[l][w]
+      #print (f"{w}:{l} = {item}")
+
+      if (item == "@"):
+        #print ("Checking...")
+
+        #iterate throught nw,n,ne,w,e,sw,s,se
+        near_count = 0
+        for nsi in [-1, 0, 1]:
+          for ewi in [-1, 0, 1]:
+
+            cl = l + nsi
+            cw = w + ewi
+            # print(f"### {cw}:{cl}")
+
+            if (cw >= 0 and cw < width and cl >= 0 and cl < length):
+              if not (nsi == 0 and ewi == 0):
+                check_item = arr[cl][cw]
+                #print(f"... {cw}:{cl} = {check_item}")
+                if check_item == "@":
+                  near_count += 1
+        
+        if near_count < 4:
+          #print (f"{w}:{l} is movable")
+          move_arr[l][w] = "x"
+          movable_count += 1
+          keep_going = True
+      #else:
+        #print ("No Need to check")
+
+  arr = copy.deepcopy(move_arr)
+
+print (f"Movable Count = {movable_count}")
+
+for ll in move_arr:
+  print("".join(ll))
+    
